702beab777aa343ea052f6f417c7b7485a223fdc
Eric Gesell <gesell.eric@gmail.com>
2013-09-28 22:13:37 -0500

    updated to support arbitrary line counter functions

--- src/vulture/core.clj
+++ src/vulture/core.clj
@@ -1,26 +1,41 @@
 (ns vulture.core)
 
 
-(def line-processors (atom {}))
+(def line-processors (atom () ))
+
+(defn generate-counter [processor-key regex]
+  (fn [line]
+    (let [match? (re-find regex line)]
+      (if match?
+        {processor-key 1} 
+        {processor-key 0}))))
+
+(comment def line-count-processor (generate-counter :total #".*"))
+(comment def error-count-processor (generate-counter :errors #"^E .*"))
 
 
 (defn add-processor!
  "adds a processor function to the list of processors.  Each processor function will be applied to each line in the analyzed log.
  processors-atom --> an atom containing a map of processor functions
- processor-name --> the name of the function.  This will be the key in the results map.
- processor-function --> a function of the form (regex line) --> any" 
-  [processors-atom processor-name processor-function]
-  (swap! processors-atom merge {processor-name processor-function}))
+ processor-function --> a function of the form (f [line]) --> {:name 0}" 
+  [processors-atom processor-function]
+  (swap! processors-atom conj processor-function))
 
 (defn read-file [file-name]
   )
 
+(defn run-processors [processors line]
+  (map (fn [processor]
+         (processor line))
+       processors))
+
 (defn apply-processors [processors lines]
   (reduce (fn [results line]
-            (merge results {:total (inc (:total results))}))
-          {:total 0}
+            (let [combined-map (into {} (run-processors processors line))]
+              (merge-with + results combined-map)))
+          {}
           lines))
 
-(defn process-file [file-name]
+(defn process-file [processors file-name]
   (let [data-stream (read-file file-name)]
-    (apply-processors line-processors data-stream)))
+    (apply-processors @processors data-stream)))
--- test/vulture/core_test.clj
+++ test/vulture/core_test.clj
@@ -4,13 +4,21 @@
 
 (facts "about `add-processor`"
        (fact "calling add-processor returns the line processor map"
-             (add-processor! line-processors :processor ...processor...) => {:processor ...processor...})
+             (add-processor! line-processors ...processor...) => (contains [...processor...]))
        (fact "anything added will be in the line-processors map"
-             (:processor @line-processors) => ...processor...))
+             @line-processors => (contains ...processor...)))
+
+(facts "about `run-processors`"
+       (against-background [(before :contents (do
+                                               (reset! line-processors ())
+                                               (add-processor! line-processors (generate-counter :total #".*"))
+                                               (add-processor! line-processors (generate-counter :errors #"^E "))))] 
+                           (fact "returns a vector of maps"
+                                 (run-processors @line-processors "E line 1") => (contains {:errors 1} {:total 1}))))
 
 (facts "about `process-file`"
        (fact "Returns the total line count, even if no processors are specified"
-             (process-file ...file-name...) => {:total 3}
+             (process-file line-processors ...file-name...) => (contains {:total 3} {:errors 2})
              (provided
-               (read-file ...file-name...) => (seq ["line 1" "line 2" "line 3"] ))))
+               (read-file ...file-name...) => (seq ["E line 1" "W line 2" "E line 3"]))))
 
